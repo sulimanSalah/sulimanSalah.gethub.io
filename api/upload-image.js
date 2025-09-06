e0baecbd3f4b8978a7db95d7b2f7a803f2f91083
sulimanSalah <sulimansalih3344@yahoo.com>
2025-09-06 11:35:52 +0200

    Update upload-image.js

--- api/upload-image.js
+++ api/upload-image.js
@@ -1,1 +1,26 @@
-هنا سيكون كود
+import { put } from '@vercel/blob';
+
+export default async function POST(request) {
+  try {
+    const file = request.files.get('file');
+
+    if (!file) {
+      return new Response(JSON.stringify({ error: 'No file uploaded.' }), {
+        status: 400,
+      });
+    }
+
+    const blob = await put(file.name, file, {
+      access: 'public', // يجعل الملفات قابلة للوصول للجميع
+    });
+
+    return new Response(JSON.stringify(blob), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    return new Response(JSON.stringify({ error: 'Failed to upload file.' }), {
+      status: 500,
+    });
+  }
+}
